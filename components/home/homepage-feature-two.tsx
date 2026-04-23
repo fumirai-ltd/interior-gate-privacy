224cd4f4be8c7f1020fcc038c613b057ec6c1e5c
dntrieunguyenfumirai <nguyen.dang.nguyen.trieu@fumirai.ltd>
2026-04-23 17:13:53 +0700

    FEAT: update image

--- components/home/homepage-feature-two.tsx
+++ components/home/homepage-feature-two.tsx
@@ -27,7 +27,7 @@ export const HomepageFeatureTwo = () => {
 
         <AnimatedSection direction="left" delay={0.08} className="relative">
           <Image
-            src="/assets/img/space-chat.webp"
+            src=""
             alt={t("imageAlt")}
             width={960}
             height={768}
